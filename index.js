d6198b1b88a02af7851ce62a910369210712dd12
fiixed <fiixed@gmail.com>
2020-10-14 10:20:53 -0400

    styling

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded',()=> {
     let renderMovies = (movieArray) => {
         let movieHTML = movieArray.map((currentMovie) => {
-             return `<div class="card movie" style="width: 18rem">
+             return `<div class="card movie">
             <img class="card-img-top poster" src="${currentMovie.Poster}" alt="" srcset="" />
             <h5 class="card-title title">${currentMovie.Title}</h5>
             <h6 class="release-date">${currentMovie.Year}</h6>
-            <a href="#" class="btn btn-primary">Add!</a>
+            <a href="#" class="btn btn-primary add">Add!</a>
           </div>`;
         });
-        return movieHTML.join(" ");
+        return movieHTML.join("");
+        
+        
     };
     document.querySelector(".movies-container").innerHTML = renderMovies(movieData);
 });
