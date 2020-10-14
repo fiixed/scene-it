1dcc9ca00d1b5f2758923c930500e5cade7b3334
fiixed <fiixed@gmail.com>
2020-10-14 10:28:50 -0400

    finish card styling

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ document.addEventListener('DOMContentLoaded',()=> {
         let movieHTML = movieArray.map((currentMovie) => {
              return `<div class="card movie">
             <img class="card-img-top poster" src="${currentMovie.Poster}" alt="" srcset="" />
+            <div id="heading">
             <h5 class="card-title title">${currentMovie.Title}</h5>
             <h6 class="release-date">${currentMovie.Year}</h6>
+            </div>
             <a href="#" class="btn btn-primary add">Add!</a>
           </div>`;
         });
